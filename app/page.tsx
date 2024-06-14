1a6375b3a4c7b6066b2a5e632aa6179744cf8139
Mohammed Zeeshan <work.zeeshan@gmail.com>
2024-06-15 00:39:25 +0530

    .

--- app/page.tsx
+++ app/page.tsx
@@ -11,6 +11,7 @@ import {
 } from "@radix-ui/themes";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { ChatBubble } from "./components/chat-bubble";
 
 const Home = () => {
   const [selectedValue, setSelectedValue] = useState<string | null>(null);
@@ -25,7 +26,7 @@ const Home = () => {
 
   return (
     <Flex minHeight={"100vh"} direction={"column"}>
-      <Flex justify={"between"} align={"center"} p={"5"}>
+      <Flex justify={"between"} align={"start"} p={"5"} pb={"8"}>
         <IconButton variant="ghost">
           <Cross1Icon width="18" height="18" />
         </IconButton>
@@ -33,8 +34,11 @@ const Home = () => {
           Start studying <RocketIcon />
         </Button>
       </Flex>
+      <Flex>
+        <ChatBubble>What year are you in? </ChatBubble>
+      </Flex>
       <Flex align={"center"} justify={"center"} flexGrow={"1"}>
-        <Flex direction={"column"} gap="3" maxWidth="200px">
+        <Flex direction={"column"} gap="3" maxWidth="400px">
           <RadioCards.Root
             name="isFinalYear"
             onValueChange={(value) => setSelectedValue(value)}
@@ -44,20 +48,28 @@ const Home = () => {
           </RadioCards.Root>
         </Flex>
       </Flex>
-      <Flex justify={"between"} align={"center"} p={"5"}>
-        <Button variant="solid" radius="medium" size={"3"}>
-          <ArrowLeftIcon width="18" height="18" />
-          Back to study
-        </Button>
-        <Button
-          variant="outline"
-          disabled={selectedValue === null}
-          size={"3"}
-          onClick={handleQuizStart}
-        >
-          Continue
-        </Button>
-      </Flex>
+      <Box className="mx-auto">
+        <Flex justify={"between"} align={"center"} p={"5"} gap={"4"}>
+          <Button
+            variant="solid"
+            radius="medium"
+            size={"3"}
+            disabled
+            onClick={() => router.back()}
+          >
+            <ArrowLeftIcon width="18" height="18" />
+            Back to study
+          </Button>
+          <Button
+            variant="outline"
+            disabled={selectedValue === null}
+            size={"3"}
+            onClick={handleQuizStart}
+          >
+            Continue
+          </Button>
+        </Flex>
+      </Box>
     </Flex>
   );
 };
