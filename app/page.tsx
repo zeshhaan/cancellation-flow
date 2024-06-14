d28f79d1792ad6aa5f10f84f611e852fb7526e48
Mohammed Zeeshan <work.zeeshan@gmail.com>
2024-06-14 17:05:41 +0530

    design the quiz layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,22 @@
 "use client";
+import { ArrowLeftIcon, Cross1Icon, RocketIcon } from "@radix-ui/react-icons";
+import {
+  Box,
+  Button,
+  Container,
+  Flex,
+  Grid,
+  IconButton,
+  RadioCards,
+} from "@radix-ui/themes";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 const Home = () => {
+  const [selectedValue, setSelectedValue] = useState<string | null>(null);
   const router = useRouter();
-  const handleQuizStart = (isFinalYear: boolean) => {
-    if (isFinalYear) {
+  const handleQuizStart = () => {
+    if (selectedValue === "6") {
       router.push("/quiz/final-year");
     } else {
       router.push("/quiz/free-access");
@@ -12,11 +24,41 @@ const Home = () => {
   };
 
   return (
-    <div>
-      <h1>Are you a final year student?</h1>
-      <button onClick={() => handleQuizStart(true)}>Yes</button>
-      <button onClick={() => handleQuizStart(false)}>No</button>
-    </div>
+    <Flex minHeight={"100vh"} direction={"column"}>
+      <Flex justify={"between"} align={"center"} p={"5"}>
+        <IconButton variant="ghost">
+          <Cross1Icon width="18" height="18" />
+        </IconButton>
+        <Button variant="solid" radius="medium">
+          Start studying <RocketIcon />
+        </Button>
+      </Flex>
+      <Flex align={"center"} justify={"center"} flexGrow={"1"}>
+        <Flex direction={"column"} gap="3" maxWidth="200px">
+          <RadioCards.Root
+            name="isFinalYear"
+            onValueChange={(value) => setSelectedValue(value)}
+          >
+            <RadioCards.Item value="5">5th year</RadioCards.Item>
+            <RadioCards.Item value="6">6th year</RadioCards.Item>
+          </RadioCards.Root>
+        </Flex>
+      </Flex>
+      <Flex justify={"between"} align={"center"} p={"5"}>
+        <Button variant="solid" radius="medium" size={"3"}>
+          <ArrowLeftIcon width="18" height="18" />
+          Back to study
+        </Button>
+        <Button
+          variant="outline"
+          disabled={selectedValue === null}
+          size={"3"}
+          onClick={handleQuizStart}
+        >
+          Continue
+        </Button>
+      </Flex>
+    </Flex>
   );
 };
 
